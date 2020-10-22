11e617a180cb2e102f867fc83e6df7e5239fded4
Benni Mack <benni@typo3.org>
2020-10-22 14:44:02 +0200

    [BUGFIX] Always include the parent page when flushing caches

--- Classes/Hooks/DataHandlerHook.php
+++ Classes/Hooks/DataHandlerHook.php
@@ -14,8 +14,6 @@ namespace B13\Menus\Hooks;
 
 use TYPO3\CMS\Core\Cache\CacheManager;
 use TYPO3\CMS\Core\Cache\Frontend\FrontendInterface;
-use TYPO3\CMS\Core\Database\ConnectionPool;
-use TYPO3\CMS\Core\Database\Query\Restriction\DeletedRestriction;
 use TYPO3\CMS\Core\DataHandling\DataHandler;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
@@ -50,31 +48,13 @@ class DataHandlerHook
             return;
         }
         $pageId = (int)$params['uid_page'];
-        // If the current page has subpages, it can be flushed directly
-        if ($this->hasSubpages($pageId)) {
-            $menuTag = 'menuId_' . $pageId;
-        } else {
-            // Page is a "leave" in the tree, so flush the menu ID of the parent page
-            $parentPageId = $dataHandler->getPID('pages', $pageId);
-            $menuTag = 'menuId_' . $parentPageId;
+        $menuTags = ['menuId_' . $pageId];
+        // Clear caches of the parent page as well (needed when moving records)
+        $parentPageId = $dataHandler->getPID('pages', $pageId);
+        if ($parentPageId > 0) {
+            $menuTags[] = 'menuId_' . $parentPageId;
         }
-        $this->cacheHash->flushByTag($menuTag);
-        $this->cachePages->flushByTag($menuTag);
-    }
-
-    protected function hasSubpages(int $pageId): bool
-    {
-        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('pages');
-        $queryBuilder->getRestrictions()
-            ->removeAll()
-            ->add(GeneralUtility::makeInstance(DeletedRestriction::class));
-        $result = $queryBuilder
-            ->select('uid')
-            ->from('pages')
-            ->where($queryBuilder->expr()->eq('pid', $queryBuilder->createNamedParameter($pageId, \PDO::PARAM_INT)))
-            ->setMaxResults(1)
-            ->execute()
-            ->fetch();
-        return !(empty($result));
+        $this->cacheHash->flushByTags($menuTags);
+        $this->cachePages->flushByTags($menuTags);
     }
 }
