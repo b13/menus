027026a871749137b9cb6649e7c19d095ba2e65e
Achim Fritz <af@achimfritz.de>
2026-04-22 08:12:54 +0200

    [RELEASE] 2.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,14 +4,14 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Menus',
     'description' => 'Various Menu functionality for TYPO3 Frontend',
     'category' => 'fe',
-    'version' => '1.1.3',
+    'version' => '2.0.0',
     'state' => 'stable',
     'author' => 'Benni Mack',
     'author_email' => 'typo3@b13.com',
     'author_company' => 'b13 GmbH',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-14.4.99',
+            'typo3' => '12.4.0-14.3.99',
         ],
     ],
 ];
